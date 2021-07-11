6b53b46bde66cbe4e95c0a4785b038dd64ebd2c0
Ilya.Valchanka <Ilya.Valchanka@thomsonreuters.com>
2021-07-11 18:27:16 +0300

    Setup token based authentication.

--- AuthenticationServiceConsumer/Startup.cs
+++ AuthenticationServiceConsumer/Startup.cs
@@ -1,9 +1,13 @@
+using System;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace AuthenticationServiceConsumer
@@ -31,6 +35,40 @@ namespace AuthenticationServiceConsumer
             {
                 o.GroupNameFormat = "'v'VVV";
                 o.SubstituteApiVersionInUrl = true;
+            });
+
+            // The list of the most popular authentication methods:
+            // * Cookie authentication
+            // * Bearer token authentication
+            // * Basic authentication(the sender sends username and password)
+            // * API keys
+            // * OAuth 2
+            // * OpenAPI
+
+            services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                var secretKey = Configuration.GetValue<string>("authorization:secretkey");
+
+                // It teaches the filter how it should validate the token.
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    // We were signing it with a secret key.
+                    // The issuer signing key has to be the same as the signing key that we use to create a token in the TokenManager.
+                    ValidateIssuerSigningKey = true,
+                    // The secret key below has to be equal to the secret key that we used in the TokenManager.
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey)),
+                    // Since we configured the expiration of the token we also want to validate lifetime.
+                    ValidateLifetime = true,
+                    // Tipically if we implement OAuth 2 solution we have the concept of issuer and audience.
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    // The line below helps us to validate the lifetime.
+                    ClockSkew = TimeSpan.Zero
+                };
             });
 
             services.AddControllers();
